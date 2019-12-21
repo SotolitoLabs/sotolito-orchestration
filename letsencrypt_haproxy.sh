adaab8ff40b699a7dd99d375f01c442e9e92d609
Ivan Chavero <ichavero@redhat.com>
2019-12-21 17:52:15 -0600

    Add script for generating fullchain haproxy certs

---
+++ letsencrypt_haproxy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LE_PATH="/etc/letsencrypt/live"
+
+if [[ "${1}" == ""]]; then
+  echo "Missing domain argument"
+  exit
+fi
+
+CERT_PATH="${LE_PATH}/${1}"
+
+echo "Generating letsencrypt cert for haproxy"
+cd $CERT_PATH
+cat privkey.pem > haproxy_fullchain.pem 
+cat fullchain.pem  >> haproxy_fullchain.pem 
+sytemctl reload haproxy

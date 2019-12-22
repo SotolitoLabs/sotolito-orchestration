745ec056d9964f7909189f631fa55b5bffcf741c
Ivan Chavero <ichavero@chavero.com.mx>
2019-12-21 17:04:59 -0700

    Add haproxy ssl setup in letsencrypt script

--- letsencrypt/letsencrypt_haproxy.sh
+++ letsencrypt/letsencrypt_haproxy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LE_PATH="/etc/letsencrypt/live"
+HAPROXY_PATH="/etc/ssl/haproxy"
 
 if [[ "${1}" == "" ]]; then
   echo "Missing domain argument"
@@ -13,4 +14,5 @@ echo "Generating letsencrypt cert for haproxy"
 cd $CERT_PATH
 cat privkey.pem > haproxy_fullchain.pem 
 cat fullchain.pem  >> haproxy_fullchain.pem 
+ln -s "${CERT_PATH}/haproxy_fullchain.pem" "${HAPROXY_PATH}/${1}"
 systemctl reload haproxy
